841917fb93cd9b27f3ade26454f7e8f9437d1b5c
swatichauhan16997 <swatichauhan16997@gmail.com>
2019-02-03 16:55:17 +0530

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: "homes#index"
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
